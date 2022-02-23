93d5f38cd84deb403f881d82e8bc9920159d1c87
Gameyz <Example@mail.com>
2022-02-23 21:47:15 +0800

    eslint

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "./components/HelloWorld.vue"
 const a = () => {
-  return "OK";
-};
+  return "OK"
+}
 </script>
 
 <template>
--- src/util/request.ts
+++ src/util/request.ts
@@ -1,3 +1,62 @@
-import axios from "axios";
+import axios, {AxiosInstance, AxiosRequestConfig, AxiosResponse} from "axios"
 
-console.log("OK",axios);
+interface HData<T> {
+  data: T,
+  returnCode: string;
+  success: boolean;
+}
+
+class HRequest {
+  config: AxiosRequestConfig
+  instance: AxiosInstance
+
+  constructor(options: AxiosRequestConfig) {
+    this.config = options
+    this.instance = axios.create(options)
+  }
+
+  // 类型参数的作用，T决定AxiosResponse实例中data的类型
+  request<T = any>(config: AxiosRequestConfig): Promise<T> {
+    return new Promise((resolve, reject) => {
+      this.instance
+        .request<any, HData<T>>(config)
+        .then((res) => {
+          resolve(res.data)
+        })
+        .catch((err) => {
+          reject(err)
+        })
+    })
+  }
+
+  get<T = any>(config: AxiosRequestConfig): Promise<T> {
+    return this.request({...config, method: "GET"})
+  }
+
+  post<T = any>(config: AxiosRequestConfig): Promise<T> {
+    return this.request({...config, method: "POST"})
+  }
+
+  delete<T = any>(config: AxiosRequestConfig): Promise<T> {
+    return this.request({...config, method: "DELETE"})
+  }
+
+  patch<T = any>(config: AxiosRequestConfig): Promise<T> {
+    return this.request({...config, method: "PATCH"})
+  }
+}
+
+
+
+interface InterceptorHooks {
+  requestInterceptor?: (config: AxiosRequestConfig) => AxiosRequestConfig;
+  requestInterceptorCatch?: (error: any) => any;
+  responseInterceptor?: (response: AxiosResponse) => AxiosResponse;
+  responseInterceptorCatch?: (error: any) => any;
+}
+interface HRequestConfig extends AxiosRequestConfig {
+  interceptorHooks?: InterceptorHooks;
+}
+
+
+export default HRequest
